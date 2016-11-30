1cdd842794ef267ff0ecd05a450471e03c49d141
Anzal Beg <anzal.b@HCL.COM>
2016-11-30 15:55:06 +0530

    compile my code

--- start/chaincode_start.go
+++ start/chaincode_start.go
@@ -103,13 +103,13 @@ func (t *SimpleChaincode) Query(stub shim.ChaincodeStubInterface, function strin
 func (t *SimpleChaincode) read(stub shim.ChaincodeStubInterface, args []string) ([]byte, error) {
     var key, jsonResp string
     var err error
-
+ 
     if len(args) != 1 {
         return nil, errors.New("Incorrect number of arguments. Expecting name of the key to query")
     }
 
     key = args[0]
-    valAsbytes, err := stub.GetState(key)
+    valAsbytes, err := stub.GetState( )
     if err != nil {
         jsonResp = "{\"Error\":\"Failed to get state for " + key + "\"}"
         return nil, errors.New(jsonResp)

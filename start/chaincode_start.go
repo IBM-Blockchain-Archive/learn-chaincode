5579713a0065ad07a23a931fbd7d869d6cc03018
dayhunter <patis.pn@gmail.com>
2016-12-10 20:09:53 +0700

    Compiled my code

--- start/chaincode_start.go
+++ start/chaincode_start.go
@@ -61,7 +61,7 @@ func (t *SimpleChaincode) Invoke(stub shim.ChaincodeStubInterface, function stri
 
 // Query is our entry point for queries
 func (t *SimpleChaincode) Query(stub shim.ChaincodeStubInterface, function string, args []string) ([]byte, error) {
-	fmt.Println("query is running " + function)
+	fmt.Println("query is running ..." + function)
 
 	// Handle different functions
 	if function == "dummy_query" {											//read a variable

e827eb7be4299f65cf12548ca4b708f7bf014a87
dayhunter <patis.pn@gmail.com>
2017-02-20 16:09:57 +0800

    test

--- finished/chaincode_finished.go
+++ finished/chaincode_finished.go
@@ -161,7 +161,7 @@ func (t *SimpleChaincode) Invoke(stub shim.ChaincodeStubInterface, function stri
 // ============================================================================================================================
 func (t *SimpleChaincode) Query(stub shim.ChaincodeStubInterface, function string, args []string) ([]byte, error) {
 	fmt.Println("query is running " + function)
-	logger.Debug("test debug a query")
+	logger.Debugf("test debug a query")
 	// Handle different functions
 	if function == "read" {													//read a variable
 		return t.read(stub, args)

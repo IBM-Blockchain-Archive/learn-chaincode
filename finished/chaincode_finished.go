28c108a4665a5573506fccd414d7456a25546e32
jdseo <jae.duk.seo@ryerson.ca>
2017-03-11 15:31:30 -0500

    sfin

--- finished/chaincode_finished.go
+++ finished/chaincode_finished.go
@@ -37,7 +37,7 @@ func main() {
 // Init resets all the things
 func (t *SimpleChaincode) Init(stub shim.ChaincodeStubInterface, function string, args []string) ([]byte, error) {
 	if len(args) != 1 {
-		return nil, errors.New("Incorrect number of arguments. Expecting 1")
+		return nil, errors.New("Incorrect number of arguments. Expecting changednsjkandkjsankj")
 	}
 
 	err := stub.PutState("hello_world", []byte(args[0]))

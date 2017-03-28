50b361313213a3615afb6d7f30d22f76978ca81f
fabianromeroq <fromero@assist.com.co>
2017-03-28 09:12:29 -0500

    compiled my code

--- start/chaincode_start.go
+++ start/chaincode_start.go
@@ -22,7 +22,7 @@ import (
 	"fmt"
 	"strconv"
 
-	"github.com/hyperledger/fabric/core/chaincode/shim"
+	"github.com/hyperledger/fabric/tree/master/core/chaincode/shim"
 )
 
 // SimpleChaincode example simple Chaincode implementation

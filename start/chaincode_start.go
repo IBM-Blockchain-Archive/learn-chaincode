32cd78685087f0cb38b32ff9f1bc04e2499e4994
徐若冰 <robinxu_1981@hotmail.com>
2017-04-10 15:40:09 +0800

    1

--- start/chaincode_start.go
+++ start/chaincode_start.go
@@ -42,6 +42,10 @@ func (t *SimpleChaincode) Init(stub shim.ChaincodeStubInterface, function string
 	if len(args) != 1 {
 		return nil, errors.New("Incorrect number of arguments. Expecting 1")
 	}
+	err:=stub.PutState("hello_world",[]byte(args[0]))
+	if err!=nil{
+		return nil,err
+	}
 
 	return nil, nil
 }
@@ -53,6 +57,8 @@ func (t *SimpleChaincode) Invoke(stub shim.ChaincodeStubInterface, function stri
 	// Handle different functions
 	if function == "init" {													//initialize the chaincode state, used as reset
 		return t.Init(stub, "init", args)
+	}else if function=="write"{
+		return t.write(stub,args)
 	}
 	fmt.Println("invoke did not find func: " + function)					//error
 
@@ -71,4 +77,22 @@ func (t *SimpleChaincode) Query(stub shim.ChaincodeStubInterface, function strin
 	fmt.Println("query did not find func: " + function)						//error
 
 	return nil, errors.New("Received unknown function query: " + function)
+}
+
+func (t *SimpleChaincode) write(stub shim.ChaincodeStubInterface,args []string)([]byte, error) {
+		var key,value string
+		var err error
+		fmt.Println("running write()")
+
+		if len(args)!=2{
+			return nil,errors.New("Incorrect number of arguments. Expecting 2. name of the key and value to set")
+		}
+
+		key=args[0]
+		value=args[1]
+		err=stub.PutState(key,[]byte(value))
+		if err!=nil{
+			return nil,err
+		}
+		return nil,nil
 }

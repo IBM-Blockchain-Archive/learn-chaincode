7aa3969ad2b6b72ca5ec683d998acae129ef58d9
shishido-naoyuki <Naoyuki_Shishido@NIandC.co.jp>
2017-04-13 15:10:22 +0900

    Compiled my code

--- start/chaincode_start.go
+++ start/chaincode_start.go
@@ -84,9 +84,8 @@ func (t *SimpleChaincode) Query(stub shim.ChaincodeStubInterface, function strin
 	fmt.Println("query is running " + function)
 
 	// Handle different functions
-	if function == "dummy_query" {											//read a variable
-		fmt.Println("hi there " + function)						//error
-		return nil, nil;
+	if function == "read" {											//read a variable
+		return t.read(stub, args)
 	}
 	fmt.Println("query did not find func: " + function)						//error
 

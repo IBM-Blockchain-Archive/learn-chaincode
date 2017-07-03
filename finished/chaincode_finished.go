1c155fb65388de1b9f5c17ed7b88217e4ec3d450
Sushanth Varma <Sushanth@airbears2-10-142-47-211.airbears2.1918.berkeley.edu>
2017-07-03 15:51:21 +0530

    hi

--- finished/chaincode_finished.go
+++ finished/chaincode_finished.go
@@ -67,9 +67,7 @@ func (t *SimpleChaincode) Invoke(stub shim.ChaincodeStubInterface, function stri
 		return t.Init(stub, "init", args)
 	} else if function == "write" {
 		return t.write(stub, args)
-	} else if function == "hello" {
-		return t.hello(stub)
-	}
+	} 
 	fmt.Println("invoke did not find func: " + function)
 
 	return nil, errors.New("Received unknown function invocation: " + function)
@@ -84,6 +82,8 @@ func (t *SimpleChaincode) Query(stub shim.ChaincodeStubInterface, function strin
 		return t.read(stub, args)
 	} else if function == "verifyUser" {
 		return t.verifyUser(stub, args)
+	} else if function == "hello" {
+		return t.hello(stub)
 	}
 	fmt.Println("query did not find func: " + function)
 

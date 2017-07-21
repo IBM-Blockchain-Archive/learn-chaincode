6f9b16bf1aa3189218fe96b6ff1f686821628c58
girishgh88 <girishgh88@gmail.com>
2017-07-21 17:12:57 +0530

    moving shareflight sample code to aviation

--- aviation/shareflight.go
+++ aviation/shareflight.go
@@ -54,7 +54,8 @@ type FlightLeg struct {
 	AvailSeats	uint8 `json:"availSeats"`
 }
 
-func (s *SmartContract) Init(APIstub shim.ChaincodeStubInterface) ([]byte, error) {
+func (t *SmartContract) Init(stub shim.ChaincodeStubInterface, function string, args []string) ([]byte, error) {
+	fmt.Println("--> shareflight initialized...")
 	return nil, nil
 }
  
@@ -72,6 +73,55 @@ func (s *SmartContract) Invoke(APIstub shim.ChaincodeStubInterface, function str
 		return s.changeFlightOwner(APIstub, args)
 	}
 	return nil, errors.New("Invalid Smart Contract function name.")
+}
+
+func (s *SmartContract) Query(stub shim.ChaincodeStubInterface, function string, args []string) ([]byte, error) {
+	fmt.Println("query is running " + function)
+
+	// Handle different functions
+	if function == "read" { //read a variable
+		return s.read(stub, args)
+	}
+	fmt.Println("query did not find func: " + function)
+
+	return nil, errors.New("Received unknown function query: " + function)
+}
+
+func (s *SmartContract) write(stub shim.ChaincodeStubInterface, args []string) ([]byte, error) {
+	var key, value string
+	var err error
+	fmt.Println("running write()")
+
+	if len(args) != 2 {
+		return nil, errors.New("Incorrect number of arguments. Expecting 2. name of the key and value to set")
+	}
+
+	key = args[0] //rename for funsies
+	value = args[1]
+	err = stub.PutState(key, []byte(value)) //write the variable into the chaincode state
+	if err != nil {
+		return nil, err
+	}
+	return nil, nil
+}
+
+// read - query function to read key/value pair
+func (s *SmartContract) read(stub shim.ChaincodeStubInterface, args []string) ([]byte, error) {
+	var key, jsonResp string
+	var err error
+
+	if len(args) != 1 {
+		return nil, errors.New("Incorrect number of arguments. Expecting name of the key to query")
+	}
+
+	key = args[0]
+	valAsbytes, err := stub.GetState(key)
+	if err != nil {
+		jsonResp = "{\"Error\":\"Failed to get state for " + key + "\"}"
+		return nil, errors.New(jsonResp)
+	}
+
+	return valAsbytes, nil
 }
 
 func (s *SmartContract) queryFlight(APIstub shim.ChaincodeStubInterface, args []string) ([]byte, error) {
@@ -230,10 +280,10 @@ func (s *SmartContract) changeFlightOwner(APIstub shim.ChaincodeStubInterface, a
 }
 
 // The main function is only relevant in unit test mode. Only included here for completeness.
-/*func main() {
+func main() {
 	// Create a new Smart Contract
 	err := shim.Start(new(SmartContract))
 	if err != nil {
 		fmt.Printf("Error creating new Smart Contract: %s", err)
 	}
-}*/
+}
